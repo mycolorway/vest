be8f2de4b9c14bc456be4bbd36c0669d5b09af4f
farthinker <farthinker@gmail.com>
2018-10-07 00:26:25 +0800

    ADD: babel transform runtime plugin

--- templates/src/app.js
+++ templates/src/app.js
@@ -1,34 +1,34 @@
-import wxAPI from '@/modules/wx_api'
+// import wxAPI from 'vest-pocket/wx-api'
 
 App({
   onLaunch() {
-    this.checkUpdate()
+    // this.checkUpdate()
   },
 
-  checkUpdate() {
-    const updateManager = wxAPI('getUpdateManager')
-    if (updateManager) {
-      updateManager.onCheckForUpdate(res => {
-        if (res.hasUpdate) {
-          wxAPI('showLoading', {
-            title: '正在下载新版本',
-            mask: true
-          })
-        }
-      })
-
-      updateManager.onUpdateReady(() => {
-        updateManager.applyUpdate()
-      })
-
-      updateManager.onUpdateFailed(() => {
-        wxAPI('hideLoading')
-        wxAPI('showModal', {
-          title: '新版本下载失败',
-          content: '请检查你的网络状况，然后重启小程序。',
-          showCancel: false
-        })
-      })
-    }
-  }
+  // checkUpdate() {
+  //   const updateManager = wxAPI('getUpdateManager')
+  //   if (updateManager) {
+  //     updateManager.onCheckForUpdate(res => {
+  //       if (res.hasUpdate) {
+  //         wxAPI('showLoading', {
+  //           title: '正在下载新版本',
+  //           mask: true
+  //         })
+  //       }
+  //     })
+  //
+  //     updateManager.onUpdateReady(() => {
+  //       updateManager.applyUpdate()
+  //     })
+  //
+  //     updateManager.onUpdateFailed(() => {
+  //       wxAPI('hideLoading')
+  //       wxAPI('showModal', {
+  //         title: '新版本下载失败',
+  //         content: '请检查你的网络状况，然后重启小程序。',
+  //         showCancel: false
+  //       })
+  //     })
+  //   }
+  // }
 })
